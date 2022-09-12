d012588d83643073363261a649ff90c535c3744d
MaximProsh <karmark@yandex.ru>
2022-09-12 22:08:07 +0300

    Add final task 47

--- Practice/Task47/Program.cs
+++ Practice/Task47/Program.cs
@@ -1,5 +1,36 @@
-﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
 // m = 3, n = 4
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
+double[,] CreateRandomRealNumsArray(int m, int n)
+{
+    Random rnd = new Random();
+    double[,] array = new double[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = Math.Round((rnd.NextDouble()
+                        * rnd.Next(-10, 10)), 1);
+        }
+    }
+    return array;
+}
+
+void PrintTwoDArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintTwoDArray(CreateRandomRealNumsArray(3, 4));
+
