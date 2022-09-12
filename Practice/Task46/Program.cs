20942280d649966a753511609b879bb708fc088f
MaximProsh <karmark@yandex.ru>
2022-09-12 10:06:34 +0300

    Create 2D array

---
+++ Practice/Task46/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 46. Задайте двумерный массив размером m x n,
+// заполненный случайными целыми числами.
+
+int[,] CreateRandomArray()
+{
+    Random rnd = new Random();
+    int m = rnd.Next(2, 5);
+    int n = rnd.Next(2, 5);
+
+    int[,] array = new int[m,n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i,j] = rnd.Next(1,99);
+        }
+    }
+    return array;    
+}
+
+void PrintTwoDArray(int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+}
+}
+
+PrintTwoDArray(CreateRandomArray());
