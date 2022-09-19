1abb4ffbd9df4bf6a0ce4fa8cdabbb4a447fb578
MaximProsh <karmark@yandex.ru>
2022-09-19 19:32:49 +0300

    add task 63

---
+++ Practice/Task63/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 63. Задайте число N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от 1 до N.
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] f(int num)
+{
+    int[] arr = new int[num];
+    for (int i = 1; i < num + 1; i++)
+    {
+        arr[i - 1] = i;
+    }
+    return arr;
+}
+
+Console.WriteLine(string.Join(", ", f(num)));
