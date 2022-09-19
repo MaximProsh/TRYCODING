3dd06fefe375163260423d531f5be1d514459a0a
MaximProsh <karmark@yandex.ru>
2022-09-19 20:32:32 +0300

    The task 69 resolved by recursion

---
+++ Practice/Task69/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 69. Напишите программу, которая на вход принимает два числа A и B,
+// и возводит число A в целую степень B с помощью рекурсии.
+
+Console.Write("Input first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int f(int a, int b)
+{
+    if (b == 1) 
+        return a;
+    else
+        return a * f(a, b - 1);
+}
+
+int x = f(a, b);
+Console.WriteLine(x);
