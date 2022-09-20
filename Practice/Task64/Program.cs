d7ba2985bc899e1f2458646da51c8899b54336f7
MaximProsh <karmark@yandex.ru>
2022-09-20 10:34:07 +0300

    Added a solution via array

--- Practice/Task64/Program.cs
+++ Practice/Task64/Program.cs
@@ -3,16 +3,34 @@
 // M = 1; N = 5. -> ""1, 2, 3, 4, 5""
 // M = 4; N = 8. -> ""4, 6, 7, 8""
 Console.Write("Input first number: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input second number: ");
 int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int n = Convert.ToInt32(Console.ReadLine());
 
 
-string GetNumsSeries(int n, int m)
+int[] GetNumsSeriesArray(int[] array, int m, int n, int number = 1)
+// Solution via array output
 {
-    if (n < m) return $"{n}, " + GetNumsSeries(++n, m);
-    
-    else return $"{n}";
+    array[number - 1] = m;
+    if (m == n) 
+    {
+        return array;
+    }
+    else
+    {        
+        return GetNumsSeriesArray(array, m + 1, n, number + 1);
+    }
 }
 
-Console.WriteLine(GetNumsSeries(n, m));
+int[] array = new int[n - m + 1];
+Console.WriteLine(string.Join(", ", GetNumsSeriesArray(array, m, n)));
+
+
+string GetNumsSeriesString(int m, int n)
+// Solution via string output
+{
+    if (m < n) return $"{m}, " + GetNumsSeriesString(++m, n);
+    
+    else return $"{m}";
+}
+Console.WriteLine(GetNumsSeriesString(m, n));
